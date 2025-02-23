cdb7287a37e47c4fe7f215f2e83e3f35c214e035
kennethpierson <kjpierson@email.neit.edu>
2025-02-23 09:21:50 -0500

    finished lab6 code

--- Winter2025/week7/lab6_collectionsLogic.py
+++ Winter2025/week7/lab6_collectionsLogic.py
@@ -7,6 +7,16 @@
 
 
 #variable dictionary
+#rows               Empty list for the rows
+#seatA              Empty list for all A seats
+#seatB              Empty list for all B seats
+#seatC              Empty list for all C seats
+#seatD              Empty list for all D seats
+#valid_row          List containing valid rows
+#valid_seat         List containing valid seats
+#answer             Start and stop the loop
+#row_choice         Variable for user input to select a row
+#seat_choice        Variable for user input to select a seat
 
 #--IMPORTS---------------------------------------------
 import csv
@@ -20,7 +30,7 @@ def loopcontrol(): #() is empty so NO parameters; this function does not require
   #return the ans value tp be used in the base program!
   return ans
 
-def seatMap():
+def seatMap(): #function to display the seat map
     print(f"\n{'ROW':3}   {'A':3} {'B':3}   {'C':3} {'D':3}")
     print("-" * 50)
     for i in range(len(rows)):
@@ -55,23 +65,23 @@ with open("text_files/seats.csv") as csvfile:
 seatMap()
 answer = "y"
 while answer == "y":
-    row_choice = input("Enter the row you'd like to sit in [1-7]: ")
-    while row_choice not in valid_row:
+    row_choice = input("Enter the row you'd like to sit in [1-7]: ")    #User input to select a row
+    while row_choice not in valid_row:                                  #Loop to validate user input for the row 
         print("\n***INVALID ENTRY***\n~Please try again.~\n")
         row_choice = input("Enter the row you'd like to sit in [1-7]: ")
-    seat_choice = input("Enter the seat you'd like to sit in [A/B/C/D]: ").upper()
-    while seat_choice not in valid_seat:
+    seat_choice = input("Enter the seat you'd like to sit in [A/B/C/D]: ").upper()     #User input to select a seat
+    while seat_choice not in valid_seat:                                               #Loop to validate user input for the seat
         print("\n***INVALID ENTRY***\n~Please try again.~\n")
         seat_choice = input("Enter the seat you'd like to sit in [A/B/C/D]: ").upper()
-    if row_choice == "1" and seat_choice == "A" and seatA[0] == "X":
-        print(f"\nYou choose Row: {row_choice} Seat: {seat_choice}")
-        print("\nThat Seat is NOT Available! Please Try Again")
-        seatMap()
-    elif row_choice == "1" and seat_choice == "A" and seatA[0] != "X":
-        print(f"\nYou choose Row: {row_choice} Seat: {seat_choice}")
-        print("\nThat Seat is Available and has been assigned to you") 
-        seatA[0] = "X"
-        seatMap()
+    if row_choice == "1" and seat_choice == "A" and seatA[0] == "X":                     #Check if seat is already assiged
+        print(f"\nYou choose Row: {row_choice} Seat: {seat_choice}")                     #Display the users row and seat choice
+        print("\nThat Seat is NOT Available! Please Try Again")                          #Tell user seat is taken and to select a different seat
+        seatMap()                                                                        #Display seat map
+    elif row_choice == "1" and seat_choice == "A" and seatA[0] != "X":                   #Check is seat is avaiable  
+        print(f"\nYou choose Row: {row_choice} Seat: {seat_choice}")                     #Display the users row and seat choice
+        print("\nThat Seat is Available and has been assigned to you")                   #Tell user seat is avaiable and assigned to them
+        seatA[0] = "X"                                                                   #Change seatA[0] to an X
+        seatMap()                                                                        #Display the seat map with the seat marked as a X
     if row_choice == "2" and seat_choice == "A" and seatA[1] == "X":
        print(f"\nYou choose Row: {row_choice} Seat: {seat_choice}")
        print("\nThat Seat is NOT Available! Please Try Again")
@@ -315,9 +325,11 @@ while answer == "y":
         print("\nThat Seat is Available and has been assigned to you")
         seatD[6] = "X"
         seatMap()
+    #Once all seats are selected will notify the user the plane is full and exit the loop and the program    
     if seatA[0] == "X" and seatA[1] == "X" and seatA[2] == "X" and seatA[3] == "X" and seatA[4] == "X" and seatA[5] == "X" and seatA[6] == "X" and seatB[0] == "X" and seatB[1] == "X" and seatB[2] == "X" and seatB[3] == "X" and seatB[4] == "X" and seatB[5] == "X" and seatB[6] == "X" and seatC[0] == "X" and seatC[1] == "X" and seatC[2] == "X" and seatC[3] == "X" and seatC[4] == "X" and seatC[5] == "X" and seatC[6] == "X" and seatD[0] == "X" and seatD[1] == "X" and seatD[2] == "X" and seatD[3] == "X" and seatD[4] == "X" and seatD[5] == "X" and seatD[6] == "X":
         answer = "n"
         print("\nThe Plane is full there are NO more available seats!")        
     else:
-        answer = loopcontrol()
-print("\nThanks for using the program! Good Bye!\n")
+        answer = loopcontrol()                   #function to control the loop and only allow correct input
+#Good Bye message exiting program       
+print("\nThank you for using the program! Good Bye!\n")
