9ec6eeeb4f0e09b05cdfbe68b424734fbf897dad
kennethpierson <kjpierson@email.neit.edu>
2025-03-06 16:35:16 -0500

    new file

--- Winter2025/finalProject/anotherBlackjack.py
+++ Winter2025/finalProject/anotherBlackjack.py
@@ -10,7 +10,7 @@ class Card:
 
 class Deck:
     def __init__(self):
-        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        suits = ["♥", "♦", "Clubs", "Spades"]
         ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
         self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
         self.shuffle()
