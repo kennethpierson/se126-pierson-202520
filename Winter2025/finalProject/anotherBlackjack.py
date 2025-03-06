4f3e83b5bc33e87dc7e0549458d39fa307c5d1af
kennethpierson <kjpierson@email.neit.edu>
2025-03-06 17:11:27 -0500

    suits

--- Winter2025/finalProject/anotherBlackjack.py
+++ Winter2025/finalProject/anotherBlackjack.py
@@ -1,4 +1,4 @@
-import random
+import csv, random
 
 class Card:
     def __init__(self, suit, rank):
@@ -6,11 +6,11 @@ class Card:
         self.rank = rank
 
     def __str__(self):
-        return f"{self.rank} of {self.suit}"
+        return f"{self.rank}{self.suit}"
 
 class Deck:
     def __init__(self):
-        suits = ["♥", "♦", "Clubs", "Spades"]
+        suits = ["♥", "♦", "♣", "♠"]
         ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
         self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
         self.shuffle()
